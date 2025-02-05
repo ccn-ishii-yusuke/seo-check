c5da3e444c5ee281337d47ebc9d8cd219b1b0e03
石井 佑典 <ishii_yusuke@cocone.co.jp>
2025-02-05 19:46:42 +0900

    ogp設定

--- app.vue
+++ app.vue
@@ -1,3 +1,51 @@
+<script setup lang="ts">
+useHead({
+  title: 'sample',
+  meta: [
+    {
+      name: 'description',
+      content: 'sample'
+    },
+    {
+        property: 'og:title',
+        content: 'ogp sample',
+    },
+    {
+        property: 'og:description',
+        content: 'ogp sample',
+    },
+    {
+        property: 'og:image',
+        content: 'https://ccn-ishii-yusuke.github.io/seo-check/ogp.png',
+    },
+    {
+        property: 'og:url',
+        content: 'https://ccn-ishii-yusuke.github.io/seo-check/',
+    },
+    {
+        property: 'og:site_name',
+        content: 'ogp sample',
+    },
+    {
+        property: 'og:image:width',
+        content: '1200',
+    },
+    {
+        property: 'og:image:height',
+        content: '630',
+    },
+    {
+        property: 'twitter:title',
+        content: 'ogp sample'
+    },
+    {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+    },
+  ]
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
