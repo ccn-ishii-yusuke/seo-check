2848084b82bb13c09bbd8b645ea231f5bb56bb7b
石井 佑典 <ishii_yusuke@cocone.co.jp>
2025-02-05 19:59:42 +0900

    baseURLを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    baseURL: '/seo-check/'
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
